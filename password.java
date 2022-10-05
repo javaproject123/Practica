8f5a84217bc6ed19f50f34c70e32c53373877590
Rakesh kumar mohanty <111591636+javaproject123@users.noreply.github.com>
2022-10-05 21:08:45 +0530

    Add files via upload

---
+++ password.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class password {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int pwd;
+         System.out.println("Enter Password:= ");
+         Scanner sc=new Scanner(System.in);
+         pwd=sc.nextInt();
+         if(pwd==2022);{
+        	 System.out.println("my name: = Rakesh kumar mohanty");
+        	 System.out.println("my age : = 24");
+        	 System.out.println("my instagram id : r_a_k_e_s_h_mohanty");
+         }
+	}
+
+}
